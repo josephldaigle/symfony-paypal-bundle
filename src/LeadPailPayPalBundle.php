1d454c634a7d01439533f58a621d08b471ee5ad8
Joe Daigle <josephldaigle@yahoo.com>
2019-12-28 08:07:00 -0600

    initialize bundle

---
+++ src/LeadPailPayPalBundle.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by Joseph Daigle.
+ * Date: 12/28/19
+ * Time: 7:27 AM
+ */
+
+namespace LeadPail\PayPalBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * LeadPailPayPalBundle.
+ *
+ * @package LeadPail\PayPalBundle
+ */
+class LeadPailPayPalBundle extends Bundle
+{
+
+}
